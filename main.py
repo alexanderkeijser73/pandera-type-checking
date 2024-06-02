d12e18a9072ecdff7bf415bb9a66170a18e755d1
alexander.keijser <alexanderkeijser73@gmail.com>
2024-06-02 15:02:25 +0900

    Initial version working

---
+++ main.py
@@ -0,0 +1,99 @@
+import dataclasses
+
+import pandas as pd
+import pandera as pa
+import pydantic
+
+from pandera.typing import Series, DataFrame
+
+class Schema(pa.DataFrameModel):
+    name: Series[str]
+    age: Series[int]
+
+class Schema2(pa.DataFrameModel):
+    banana: Series[int]
+
+class Schema3(Schema2):
+    apple: Series[int]
+
+
+bla: DataFrame[Schema] = DataFrame[Schema](pd.DataFrame.from_dict({
+    "name": ["Alice", "Bob", "Charlie"],
+    "age": [25, 30, 35],
+}))
+
+# TODO: can't refactor-> change name of variable with Pycharm :(
+print(bla.age)
+# TODO:
+print(bla.banana)
+print(bla.potato)
+
+
+
+def func(df: DataFrame[Schema2]) -> DataFrame[Schema3]:
+    return df.pipe(DataFrame[Schema3])
+
+func(bla)
+
+
+# @dataclasses.dataclass
+# class Person:
+#     name: str
+#     agel: int
+#
+# def get_person():
+#     return Person(name="Alice", agel=25)
+#
+# dataclass_person = get_person()
+# # this does complain
+# print(dataclass_person.size)
+# # TODO: can refactor-> change name of variable with Pycharm :)
+# print(dataclass_person.agel)
+#
+#
+# ##########################################################
+# # pydantic
+# ##########################################################
+#
+# class Person2(pydantic.BaseModel):
+#     name: str
+#     agel: int
+#
+# person2 = Person2(name="Alice", age=25)
+# # TODO: can refactor-> change name of variable with Pycharm :)
+# print(person2.agel)
+# # this does complain (not Pycharm, but Mypy does)
+# print(person2.size)
+#
+#
+# ##########################################################
+# # sqlalchemy
+# ##########################################################
+# from sqlalchemy import Column, Integer, String, select
+# from sqlalchemy.orm import declarative_base
+#
+# # "Base" is a class that is created dynamically from the
+# # declarative_base() function
+# Base = declarative_base()
+#
+#
+# class User(Base):
+#     __tablename__ = "user"
+#
+#     id = Column(Integer, primary_key=True)
+#     name = Column(String)
+#
+#
+# # "some_user" is an instance of the User class, which
+# # accepts "id" and "name" kwargs based on the mapping
+# some_user = User(id=5, name="user")
+#
+# # it has an attribute called .name that's a string
+# print(f"Username: {some_user.name}")
+# bla = some_user.banana
+#
+# # a select() construct makes use of SQL expressions derived from the
+# # User class itself
+# select_stmt = select(User).where(User.id.in_([3, 4, 5])).where(User.name.contains("s"))
+#
+#
---
+++ run_mypy.py
@@ -0,0 +1,4 @@
+from mypy.api import run
+
+if __name__ == '__main__':
+    run(['main.py'])
